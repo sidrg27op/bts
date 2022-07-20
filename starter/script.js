936d5e44d851e77a4eb59f274d19ddc7fbef99b2
siddharth raghav <thakurraghav27@gmail.com>
2022-07-20 14:03:14 +0530

    this

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+///////////////////////////////////////
+// this in Practice
+
+var op = 'ds';
+
+const pp = {
+  nuni: 'pp',
+  cll: function () {
+    console.log(this.nuni);
+  },
+};
+
+pp.cll();
+
+const mv = {
+  nuni: 'pppppp',
+};
+
+mv.cll = pp.cll;
+
+mv.cll();
