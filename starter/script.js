bc6ba84fd1322e43c61092acb60a203d5e7f045f
siddharth raghav <thakurraghav27@gmail.com>
2022-07-22 12:06:59 +0530

    bts completed

--- starter/script.js
+++ starter/script.js
@@ -7,8 +7,14 @@ var op = 'ds';
 
 const pp = {
   nuni: 'pp',
+  tuni: 'iii',
   cll: function () {
     console.log(this.nuni);
+
+    const mill = () => {
+      console.log(this.tuni);
+    };
+    mill();
   },
 };
 
@@ -16,8 +22,21 @@ pp.cll();
 
 const mv = {
   nuni: 'pppppp',
+  tuni: 'ooo',
 };
 
 mv.cll = pp.cll;
 
 mv.cll();
+
+// primitive & referencec
+
+let age = 30;
+let ageo = age;
+
+age = 31;
+
+console.log(age);
+console.log(ageo);
+
+const opyo = {};
